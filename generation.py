36cc71b4067b0fbfcaa5e66c4be73db8777c8c42
florent <florent.houbart@gmail.com>
2024-04-04 11:40:18 +0200

    changement texte du titre

--- generation.py
+++ generation.py
@@ -1,6 +1,6 @@
 fichier = open("index.html","w")
 
-fichier.write("<html><body><h1>Site web généré</h1>")
+fichier.write("<html><body><h1>Site web automatique</h1>")
 fichier.write("<p>Ceci est le site du client <b>client1</b></p>")
 fichier.write("</body></html>")
 
